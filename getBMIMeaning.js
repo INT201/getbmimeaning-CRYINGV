df44f0a9ce561ce65d25896551327301e9900a69
Thanaphat Julmeewong <jobhandsomes@gmail.com>
2022-09-13 23:37:04 +0700

    Done

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,19 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  console.log('Working...')
+    let calBMI = weight /(height^2)
+    return calBMI;
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  let eiei = calculateBMI(weight, height)
+  if (eiei < 18.5) {
+    return "Underweight"
+  } else if (18.5 <= eiei && eiei <= 24.9) {
+    return "Normal weight"
+  } else {
+    return "Overweight"
+  }
+
 }
 module.exports = getBMIMeaning
+
